9d16b04acbe3dfc732277ad14e4abf193e2ef98b
Emmanuel Ayeniko <108879983+NIKO-DEV06@users.noreply.github.com>
2025-07-30 01:19:14 +0100

    deploy

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  const host = `0.0.0.0`;
-
+  const host = '0.0.0.0';
   await app.listen(port, host);
 }
 bootstrap();
